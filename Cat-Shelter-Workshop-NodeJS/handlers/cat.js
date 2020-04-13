331c97d5ab58eb5404caa6048fb43e81d4fe3632
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-04-14 02:31:35 +0300

    Made the Initial structure, added some handlers and two data files

---
+++ Cat-Shelter-Workshop-NodeJS/handlers/cat.js
@@ -0,0 +1,9 @@
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+const cats = require('../data/cats.json');
+const breeds = require('../data/breeds.json');
+const qs = require('querystring');
+const formidable = require('formidable');
+
+
---
+++ Cat-Shelter-Workshop-NodeJS/handlers/static-files.js
@@ -0,0 +1,46 @@
+const url = require('url');
+const fs = require('fs');
+
+function getContentType(url) {
+    if (url.endsWith('css')) {
+        return 'text/css';
+    } else if (url.endsWith('html')) {
+        return 'text/html';
+    } else if (url.endsWith('js')) {
+        return 'text/javascript';
+    } else if (url.endsWith('png')) {
+        return 'image/png';
+    } else if (url.endsWith('jpg') || url.endsWith('jpeg') || url.endsWith('jfif') || url.endsWith('pjpeg') || url.endsWith('pjp')) {
+        return 'image/jpeg';
+    } else if (url.endsWith('ico') || url.endsWith('cur')) {
+        return 'image/x-icon	'
+    }
+}
+module.exports = (req, res) => {
+    const pathname = url.parse(req.url).pathname;
+
+    if (pathname.startsWith('/content') && req.method === 'GET') {
+
+        fs.readFile(`./${pathname}`, 'utf-8', (error, data) => {
+            if (error) {
+                console.log(error);
+
+                res.writeHead(404, {
+                    'Content-Type': 'text/plain'
+                });
+                res.write('Whoops! File not found!');
+                res.end();
+                return;
+            }
+            console.log(pathname);
+
+            res.writeHead(200, {
+                'Content-Type': getContentType(pathname)
+            });
+            res.write(data);
+            res.end();
+        });
+    } else {
+        return true;
+    }
+}
