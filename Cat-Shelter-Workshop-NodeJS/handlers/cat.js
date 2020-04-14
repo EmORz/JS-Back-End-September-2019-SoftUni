6c6cdcf28f7ca03f80e950cd03f6c6dcde242e63
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-04-14 10:16:13 +0300

    added catHandler to accessing addCat and addBreed views

--- Cat-Shelter-Workshop-NodeJS/handlers/cat.js
+++ Cat-Shelter-Workshop-NodeJS/handlers/cat.js
@@ -1,9 +1,60 @@
 const url = require('url');
 const fs = require('fs');
 const path = require('path');
-const cats = require('../data/cats.json');
-const breeds = require('../data/breeds.json');
-const qs = require('querystring');
-const formidable = require('formidable');
+// const cats = require('../data/cats.json');
+// const breeds = require('../data/breeds.json');
+// const qs = require('querystring');
+// const formidable = require('formidable');
+
+module.exports = (req, res) => {
+
+    const pathname = url.parse(req.url).pathname;
+
+    if (pathname === '/cats/add-cat' && req.method === 'GET') {
+        const filePath = path.normalize(
+            path.join(__dirname, '../views/addCat.html')
+        );
+
+        fs.readFile(filePath, (error, data) => {
+            if (error) {
+                res.writeHead(404, {
+                    'Content-Type': 'text/plain'
+                });
+                res.write('Whoops! File not found!');
+                res.end();
+                return;
+            }
+
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            });
+            res.write(data);
+            res.end();
+        });
+    } else if (pathname === '/cats/add-breed' && req.method === 'GET') {
+        const filePath = path.normalize(
+            path.join(__dirname, '../views/addBreed.html')
+        );
+
+        fs.readFile(filePath, (error, data) => {
+            if (error) {
+                res.writeHead(404, {
+                    'Content-Type': 'text/plain'
+                });
+                res.write('Whoops! File not found!');
+                res.end();
+                return;
+            }
+
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            });
+            res.write(data);
+            res.end();
+        });
+    } else {
+        return true;
+    }
+}
 
 
