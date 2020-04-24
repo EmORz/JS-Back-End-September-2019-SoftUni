1307b9c98aec3493d1b2bd180566840aab26ad9c
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-04-24 21:48:39 +0300

    added changeArticle function and write to file functionality

--- JS-Application-Exam-05042020-With-ExpressJS/models/articleModels.js
+++ JS-Application-Exam-05042020-With-ExpressJS/models/articleModels.js
@@ -6,8 +6,18 @@ class ArticleModel {
         this.articleData = require('../config/articleDb.json')
     }
 
-    get articlesCategory(){
-        return this.articleData.ariclesCategory;
+    _write(newData, resolveData) {
+        return new Promise((resolve, reject) => {
+            fs.writeFile(path.resolve('./config/articleDb.json'), JSON.stringify(newData, null, 2), (err) => {
+                if (err) { reject(err); return; }
+                this.articleData = newData;
+                resolve(resolveData);
+            });
+        });
+    }
+
+    get articlesCategory() {
+        return this.articleData.articlesCategory;
     }
 
     getAllArticles() {
@@ -16,6 +26,15 @@ class ArticleModel {
 
     getOneArticle(id) {
         return Promise.resolve(this.articleData.articles.find(({ id: articleId }) => articleId === id));
+    }
+
+    changeArticle(article) {
+        const newArticleData = {
+            "lastIndex": this.articleData.lastIndex,
+            "articles": this.articleData.articles.map(art => art.id !== article.id ? art : Object.assign(art, article)),
+            "articlesCategory": this.articleData.articlesCategory.slice(0)
+        }
+        return this._write(newArticleData, article);
     }
 }
 
