096d80e2605dbd85a586e57f234809b4d2bb71bb
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-04-24 18:59:22 +0300

    Added getter for articleCategory, getAllArticles and getOneArticle functions

--- JS-Application-Exam-05042020-With-ExpressJS/models/articleModels.js
+++ JS-Application-Exam-05042020-With-ExpressJS/models/articleModels.js
@@ -3,14 +3,20 @@ const path = require('path');
 
 class ArticleModel {
     constructor() {
-        this.data = require('../config/articleDb.json')
+        this.articleData = require('../config/articleDb.json')
+    }
+
+    get articlesCategory(){
+        return this.articleData.ariclesCategory;
     }
 
     getAllArticles() {
-        
+        return Promise.resolve(this.articleData.articles);
     }
 
     getOneArticle(id) {
-
+        return Promise.resolve(this.articleData.articles.find(({ id: articleId }) => articleId === id));
     }
 }
+
+module.exports = new ArticleModel();
