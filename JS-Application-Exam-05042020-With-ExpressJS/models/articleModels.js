07b0087fcda0d63d5861ce60584c7d1e23618ada
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-04-24 03:35:25 +0300

    Create articleModels.js

---
+++ JS-Application-Exam-05042020-With-ExpressJS/models/articleModels.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+class ArticleModel {
+    constructor() {
+        this.data = require('../config/articleDb.json')
+    }
+
+    getAllArticles() {
+        
+    }
+
+    getOneArticle(id) {
+
+    }
+}
