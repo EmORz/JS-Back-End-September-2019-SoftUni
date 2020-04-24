f0b2b32554527b9b7e5535e3393bd15e6d7bbd32
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-04-24 18:57:16 +0300

    Added getDetails, getEdit and postEdit functions and modified getAll function

--- JS-Application-Exam-05042020-With-ExpressJS/controllers/articleControllers.js
+++ JS-Application-Exam-05042020-With-ExpressJS/controllers/articleControllers.js
@@ -1,18 +1,82 @@
+const articleModel = require('../models/articleModels')
 const jwt = require('jsonwebtoken');
 
-function getAll(req, res) {
+function getAll(req, res, next) {
     jwt.verify(res.token, 'my_secret_key', function (err, data) {
         if (err) {
             res.redirect('/register');
             return;
-        } else {
-            const { isLogged } = res.body;
-            res.render('home', { isLogged });
         }
-    })
+        const { isLogged } = res.body;
+        articleModel.getAllArticles().then(articles => {
+            const content = {};
+            articleModel.articlesCategory.map(
+                category => {
+                    content[category.title] = [];
+                    articles.map(art => {
+                        if (category.descr === art.category) {
+                            content[category.title].push(art)
+                        }
+                    })
+                }
+            )
+            res.render('home.hbs', { content, isLogged });
+        }).catch(next)
+    });
 
 }
 
+function getDetails(req, res, next) {
+    jwt.verify(res.token, 'my_secret_key', function (err, data) {
+        if (err) {
+            res.redirect('/register');
+            return;
+        }
+        const useremail = data.user.email;
+        const { isLogged } = res.body;
+        const id = +req.params.id;
+        articleModel.getOneArticle(id).then(function (article) {
+            const isAuthor = useremail===article['creator-email'];
+            res.render('details.hbs', { article, isLogged, isAuthor });
+        }).catch(next)
+    });
+}
+
+function getEdit(req, res, next) {
+    jwt.verify(res.token, 'my_secret_key', function (err, data) {
+        if (err) {
+            res.redirect('/register');
+            return;
+        }
+        // const useremail = data.user.email;
+        const { isLogged } = res.body;
+        const id = +req.params.id;
+        articleModel.getOneArticle(id).then(function (article) {
+            // const isAuthor = useremail===article['creator-email'];
+            res.render('edit.hbs', { article, isLogged });
+        }).catch(next)
+    });
+}
+
+function postEdit(req, res, next) {
+    // jwt.verify(res.token, 'my_secret_key', function (err, data) {
+    //     if (err) {
+    //         res.redirect('/register');
+    //         return;
+    //     }
+    //     const { isLogged } = res.body;
+    //     const id = +req.params.id;
+    //     articleModel.getOneArticle(id).then(function (article) {
+    //         res.render('edit.hbs', { article, isLogged });
+    //     }).catch(next)
+    // });
+    res.redirect('/');
+}
+
+
 module.exports = {
-    getAll
+    getAll,
+    getDetails,
+    getEdit,
+    postEdit
 }
