cdcb88d71d2f9107a41e04c5c5af40730f540db2
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-04-29 00:10:11 +0300

    Added routes: '/create/accessory' an '/attach/accessory/:id'

--- Workshop ExpressJS And Templating Using Mongodb/config/routes.js
+++ Workshop ExpressJS And Templating Using Mongodb/config/routes.js
@@ -1,10 +1,8 @@
-const cubeControlers = require('../controllers/cubeControlers');
-const otherControlers = require('../controllers/otherControlers')
+const { cubeControlers, accessoryControlers, otherControlers } = require('../controllers');
 
 module.exports = (app) => {
 
     app.get('/', cubeControlers.getAllCubes)
-
     app.post('/', cubeControlers.searchCubes)
 
     app.get('/details/:id', cubeControlers.getCube)
@@ -12,9 +10,13 @@ module.exports = (app) => {
     app.get('/about', otherControlers.getAbout)
 
     app.get('/create', cubeControlers.getCreate)
-
     app.post('/create', cubeControlers.postCreate)
 
+    app.get('/create/accessory', accessoryControlers.getCreate)
+    app.post('/create/accessory', accessoryControlers.postCreate)
+
+    app.get('/attach/accessory/:id', accessoryControlers.getAttach)
+    app.post('/attach/accessory/:id', accessoryControlers.postAttach)
+
     app.all('*', otherControlers.notFound)
-  
 };
