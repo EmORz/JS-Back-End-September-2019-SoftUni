22ee8c034a17ca42b2c6fe8743fa5665658dec94
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-04-29 00:14:26 +0300

    Create index.js

---
+++ Workshop ExpressJS And Templating Using Mongodb/controllers/index.js
@@ -0,0 +1,5 @@
+const cubeControlers = require('./cubeControlers');
+const accessoryControlers = require('./accessoryControllers')
+const otherControlers = require('./otherControlers')
+
+module.exports = { cubeControlers, accessoryControlers, otherControlers }
