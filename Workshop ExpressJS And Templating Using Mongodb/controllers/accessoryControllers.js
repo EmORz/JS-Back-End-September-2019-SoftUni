70d207d5b636a6d7869818c71457796068896d5f
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-04-29 00:13:51 +0300

    Update accessoryControllers.js

--- Workshop ExpressJS And Templating Using Mongodb/controllers/accessoryControllers.js
+++ Workshop ExpressJS And Templating Using Mongodb/controllers/accessoryControllers.js
@@ -1,4 +1,4 @@
-const models = require('../models');
+const {accessoryModel, cubeModel} = require('../models');
 
 function getCreate(req, res, next) {
     res.render('createAccessory.hbs');
@@ -6,15 +6,15 @@ function getCreate(req, res, next) {
 
 function postCreate(req, res, next) {
     const { name, description, imageUrl } = req.body;
-    models.accessoryModel.create({ name, description, imageUrl })
+    accessoryModel.create({ name, description, imageUrl })
         .then(created => { res.redirect('/'); })
         .catch(next);
 }
 
 function getAttach(req, res, next) {
     const { id: cubeId } = req.params;
-    models.cubeModel.findById(cubeId).then(
-        cube => Promise.all([cube, models.accessoryModel.find({ cubes: { $nin: cube._id } })])
+    cubeModel.findById(cubeId).then(
+        cube => Promise.all([cube, accessoryModel.find({ cubes: { $nin: cube._id } })])
             .then(([cube, filterAccessories]) => res.render('attachAccessory.hbs', { accessories: filterAccessories, cube }))
             .catch(next));
 }
@@ -23,8 +23,8 @@ function postAttach(req, res, next) {
     const { id: cubeId } = req.params;
     const { accessory: accessoryId } = req.body;
     Promise.all([
-        models.cubeModel.updateOne({ _id: cubeId }, { $push: { accessories: accessoryId } }),
-        models.accessoryModel.updateOne({ _id: accessoryId }, { $push: { cubes: cubeId } })])
+        cubeModel.updateOne({ _id: cubeId }, { $push: { accessories: accessoryId } }),
+        accessoryModel.updateOne({ _id: accessoryId }, { $push: { cubes: cubeId } })])
         .then(() => {
             res.redirect('/')
         })
