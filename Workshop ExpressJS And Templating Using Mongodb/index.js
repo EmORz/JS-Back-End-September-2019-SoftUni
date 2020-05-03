646e7156dc7d8dedcb30a84e4eeefba1c0526a1b
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-05-03 10:42:02 +0300

    Update index.js
    Added global error handler

--- Workshop ExpressJS And Templating Using Mongodb/index.js
+++ Workshop ExpressJS And Templating Using Mongodb/index.js
@@ -10,5 +10,10 @@ dbConnector().then(() => {
     require('./config/express')(app);
     require('./config/routes')(app);
 
+    app.use(function (err, req, res, next) {
+        console.error(err);
+        res.render('500.hbs', { errorMessage: err.message });
+    });
+
     app.listen(config.port, console.log(`Listening on port ${config.port}!`));
 }).catch(console.error);
