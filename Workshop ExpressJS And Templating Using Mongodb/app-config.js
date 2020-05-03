8e51840c875d0bd4a2640bb31d3a0e772281f917
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-05-03 10:22:18 +0300

    Create app-config.js

---
+++ Workshop ExpressJS And Templating Using Mongodb/app-config.js
@@ -0,0 +1,14 @@
+const authCookieName = 'auth_cookie';
+const difficultyLevel = [
+  '1 - Very Easy',
+  '2 - Easy',
+  '3 - Medium (Standard 3x3)',
+  '4 - Intermediate',
+  '5 - Expert',
+  '6 - Hardcore'
+];
+
+module.exports = {
+  authCookieName,
+  difficultyLevel
+}
