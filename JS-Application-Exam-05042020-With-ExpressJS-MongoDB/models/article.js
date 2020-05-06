b46eed4bbf8a1fe4cd42110586bc8ef852a6967c
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-05-06 21:53:43 +0300

    Update article.js

--- JS-Application-Exam-05042020-With-ExpressJS-MongoDB/models/article.js
+++ JS-Application-Exam-05042020-With-ExpressJS-MongoDB/models/article.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const articleSchema = new mongoose.Schema({
   title: {
     type: String,
-    required: true
+    required: [true, "Article title required!"]
   },
   category: {
     type: mongoose.Types.ObjectId, ref: 'Category'
