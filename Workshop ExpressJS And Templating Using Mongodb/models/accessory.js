2c4c5ee4ebb8b08ddc555169fc8011d32b9aab0f
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-05-06 21:58:39 +0300

    Added accessory validator

--- Workshop ExpressJS And Templating Using Mongodb/models/accessory.js
+++ Workshop ExpressJS And Templating Using Mongodb/models/accessory.js
@@ -3,16 +3,41 @@ const mongoose = require('mongoose');
 const accessorySchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
-    },
-    imageUrl: {
-        type: String,
-        required: true
+        required: true,
+        minlength: [5, 'Name should be at least 5 characters long!'],
+        validate: [
+            {
+                validator: function (v) {
+                    return /^[A-Za-z0-9\s]+$/.test(v);
+                },
+                message: () => `Name should consists English letters, digits and whitespaces!`
+            }
+        ]
     },
     description: {
         type: String,
         required: true,
-        maxlength: 250
+        minlength: [20, 'Description should be at least 20 characters long!'],
+        validate: [
+            {
+                validator: function (v) {
+                    return /^[A-Za-z0-9\s]+$/.test(v);
+                },
+                message: () => `Description should consists English letters, digits and whitespaces!`
+            }
+        ]
+    },
+    imageUrl: {
+        type: String,
+        required: true,
+        validate: [
+            {
+                validator: function (v) {
+                    return /^http[s]?:\/\/.+/gi.test(v);
+                },
+                message: () => `imageUrl should starts with http:// or https://!`
+            }
+        ]
     },
     cubes: [{ type: mongoose.Types.ObjectId, ref: 'Cube' }]
 })
