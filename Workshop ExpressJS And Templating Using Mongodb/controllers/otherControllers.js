4b7cc3e43a251e54768a1b045aaffb9b4c09312f
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-05-06 21:58:13 +0300

    Refactoring

--- Workshop ExpressJS And Templating Using Mongodb/controllers/otherControllers.js
+++ Workshop ExpressJS And Templating Using Mongodb/controllers/otherControllers.js
@@ -1,14 +1,12 @@
-function getAbout(req, res) {
-    const user = req.user;
-    res.render('about.hbs', { user })
-}
-
-function notFound(req, res) {
-    const user = req.user;
-    res.render('404.hbs', { user })
-}
-
 module.exports = {
-    getAbout,
-    notFound
+    get: {
+        about: function (req, res) {
+            const user = req.user;
+            res.render('about.hbs', { user })
+        },
+        notFound: function (req, res) {
+            const user = req.user;
+            res.render('404.hbs', { user })
+        }
+    }
 }
--- Workshop ExpressJS And Templating Using Mongodb/controllers/userControllers.js
+++ Workshop ExpressJS And Templating Using Mongodb/controllers/userControllers.js
@@ -2,80 +2,74 @@ const { userModel, tokenBlackListModel } = require('../models');
 const jwt = require('../utils/jwt');
 const appConfig = require('../app-config');
 
-function getRegister(req, res, next) {
-    res.render('register.hbs');
-}
-
-function postRegister(req, res, next) {
-    const { username, password, repeatPassword } = req.body;
-    if (password !== repeatPassword) {
-        res.render('register.hbs', {
-            errors: {
-                repeatPassword: 'Password and repeat password don\'t mach'
-            }
-        });
-        return;
-    }
-    return userModel.create({ username, password }).then(() => {
-        res.redirect('/login');
-    }).catch(error => {
-        if (error.name = 'MongoError' && error.code === 11000) {
-            console.log(error)
-            res.render('register.hbs', {
-                errors: {
-                    username: 'Username already taken!'
-                }
-            });
-            return
+module.exports = {
+    get: {
+        register: function (req, res, next) {
+            res.render('register.hbs');
+        },
+        login: function (req, res, next) {
+            res.render('login.hbs');
+        },
+        logout: function (req, res) {
+            const token = req.cookies[appConfig.authCookieName];
+            tokenBlackListModel.create({ token }).then(() => {
+                res.clearCookie(appConfig.authCookieName).redirect('/');
+            })
         }
-        next(error);
-    });
-}
+    },
+    post: {
+        register: function (req, res, next) {
+            const newUser = { ...req.body };
 
-function getLogin(req, res, next) {
-    res.render('login.hbs');
-}
-
-function postLogin(req, res, next) {
-    const { username, password } = req.body;
-    userModel.findOne({ username })
-        .then(user => Promise.all([user, user.matchPassword(password)]))
-        .then(([user, match]) => {
-            if (!match) {
-                res.render('login.hbs', { message: 'Wrong password or username' });
+            if (newUser.password !== newUser.repeatPassword) {
+                const errors = {
+                    repeatPassword: {
+                        message: 'Password and repeat password don\'t mach'
+                    }
+                }
+                res.render('register.hbs', {
+                    newUser,
+                    errors
+                });
                 return;
             }
-            const token = jwt.createToken({ id: user._id });
-            res.cookie(appConfig.authCookieName, token).redirect('/')
-        })
-}
-
-function getLogout(req, res) {
-    // isLogged = false;
-    const token = req.cookies[appConfig.authCookieName];
-    tokenBlackListModel.create({ token }).then(() => {
-        res.clearCookie(appConfig.authCookieName).redirect('/');
-    })
-}
-
-function isLogged(req, res, next) {
-    const isLogged = (req.cookies && req.cookies.user && req.cookies.user.username !== null) ? true : false;
-    res.body = { isLogged }
-    next()
-}
-
-function setCookie(req, res) {
-    const user = Object.assign({}, { id: res.body.id, username: res.body.username });
-    res.cookie('user', user);
-    res.redirect('/')
-}
-
-module.exports = {
-    getRegister,
-    postRegister,
-    getLogin,
-    postLogin,
-    getLogout,
-    setCookie,
-    isLogged
+            return userModel.create({ ...newUser }).then(() => {
+                res.redirect('/login');
+            }).catch(err => {
+                if (err.name = 'ValidationError') {
+                    res.render('register.hbs', {
+                        user,
+                        errors: err.errors
+                    });
+                    return
+                }
+                next(err);
+            });
+        },
+        login: function (req, res, next) {
+            const { username, password } = req.body;
+            userModel.findOne({ username })
+                .then(user => Promise.all([user, user ? user.matchPassword(password) : false]))
+                .then(([user, match]) => {
+                    if (!match) {
+                        const errors = {
+                            password: {
+                                message: 'Wrong password or username!'
+                            }
+                        }
+                        res.render('login.hbs', {
+                            username, password, errors
+                        });
+                        return;
+                    }
+                    // if (!match) {
+                    //     res.render('500.hbs', { errorMessage: 'Wrong password or username' });
+                    //     return;
+                    // }
+                    const token = jwt.createToken({ id: user._id });
+                    res.cookie(appConfig.authCookieName, token).redirect('/')
+                })
+                .catch(err => next(err))
+        }
+    }
 }
