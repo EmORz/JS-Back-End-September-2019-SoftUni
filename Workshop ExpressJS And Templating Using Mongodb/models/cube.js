5fc6bf4bde25ca1fca2fac39d9e39f5b8444096c
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-05-06 21:58:51 +0300

    Added cube validator

--- Workshop ExpressJS And Templating Using Mongodb/models/cube.js
+++ Workshop ExpressJS And Templating Using Mongodb/models/cube.js
@@ -3,17 +3,41 @@ const mongoose = require('mongoose');
 const cubeSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        minlength: [5, 'Name should be at least 5 characters long!'],
+        validate: [
+            {
+                validator: function (v) {
+                    return /^[A-Za-z0-9\s]+$/.test(v);
+                },
+                message: () => `Name should consists English letters, digits and whitespaces!`
+            }
+        ]
     },
     description: {
         type: String,
         required: true,
-        maxlength: 250
+        minlength: [20, 'Description should be at least 20 characters long!'],
+        validate: [
+            {
+                validator: function (v) {
+                    return /^[A-Za-z0-9\s]+$/.test(v);
+                },
+                message: () => `Description should consists English letters, digits and whitespaces!`
+            }
+        ]
     },
     imageUrl: {
         type: String,
-        match: /^http[s]?:\/\/.+/gi,
-        required: true
+        required: true,
+        validate: [
+            {
+                validator: function (v) {
+                    return /^http[s]?:\/\/.+/gi.test(v);
+                },
+                message: () => `imageUrl should starts with http:// or https://!`
+            }
+        ]
     },
     difficultyLevel: {
         type: Number,
@@ -24,11 +48,5 @@ const cubeSchema = new mongoose.Schema({
     accessories: [{ type: mongoose.Types.ObjectId, ref: 'Accessory' }],
     creatorId: { type: mongoose.Types.ObjectId, ref: 'User' }
 })
-
-// cubeSchema.path('imageUrl')
-//     .validate(function () {
-//         return this.imageUrl.match(/^http[s]?:\/\/.+/gi)
-//     }, 'Image URL must start with http or https!')
-
 
 module.exports = mongoose.model('Cube', cubeSchema);
