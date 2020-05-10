eb211acb67d28f35ff7dbaef3553b0710c42ffe4
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-05-10 16:55:43 +0300

    Update course.js

--- Exams/Exam ExpressJS 19 April 2019/routers/course.js
+++ Exams/Exam ExpressJS 19 April 2019/routers/course.js
@@ -3,11 +3,13 @@ const controllers = require('../controllers');
 const auth = require('../utils/auth');
 
 router.get('/create', auth(), controllers.course.get.create);
+
 router.post('/create', auth(), controllers.course.post.create);
 
 router.get('/details/:id', auth(), controllers.course.get.details);
 
 router.get('/edit/:id', auth(), controllers.course.get.edit);
+
 router.post('/edit/:id', auth(), controllers.course.post.edit);
 
 router.get('/enroll/:id', auth(), controllers.course.get.enroll);
