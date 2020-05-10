490e3adc0279f2f3522767295efac6ca7fc8d402
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-05-10 16:54:38 +0300

    Refactoring user.js

--- Exams/Exam ExpressJS 19 April 2019/controllers/user.js
+++ Exams/Exam ExpressJS 19 April 2019/controllers/user.js
@@ -3,23 +3,29 @@ const { jwt } = require('../utils');
 const config = require('../config/config');
 
 module.exports = {
+
   get: {
     login: function (req, res, next) {
       res.render('loginPage.hbs', { pageTitle: 'Login Page' });
     },
+
     register: function (req, res, next) {
       res.render('registerPage.hbs', { pageTitle: 'Register Page' });
     },
+
     logout: function (req, res) {
       const token = req.cookies[config.cookie];
+
       models.TokenBlackList.create({ token }).then(() => {
         res.clearCookie(config.cookie).redirect('/home/');
       });
     }
   },
+
   post: {
     login: function (req, res, next) {
       const { username, password } = req.body;
+
       models.User.findOne({ username })
         .then(user => Promise.all([user, user ? user.matchPassword(password) : false]))
         .then(([user, match]) => {
@@ -27,20 +33,25 @@ module.exports = {
             const errors = {
               message: 'Wrong password or username!'
             };
+
             res.render('loginPage.hbs', { username, password, errors, pageTitle: 'Login Page' });
             return;
-          }
+          };
           const token = jwt.createToken({ id: user._id });
-          res.cookie(config.cookie, token).redirect('/home/')
+
+          res.cookie(config.cookie, token).redirect('/home/');
         })
         .catch(err => next(err));
     },
+
     register: function (req, res, next) {
       const { username, password, repeatPassword } = req.body;
+
       if (password !== repeatPassword) {
         const errors = {
           message: "Repeat password have to be the same like password."
-        }
+        };
+
         res.render('registerPage.hbs', { username, password, repeatPassword, errors, pageTitle: 'Register Page' });
         return;
       }
@@ -49,6 +60,7 @@ module.exports = {
           res.redirect('/user/login');
         })
         .catch(err => {
+
           if (err.name === 'ValidationError') {
             const hbsObject = {
               pageTitle: 'Register Page',
@@ -57,11 +69,12 @@ module.exports = {
               repeatPassword,
               errors: err.errors
             };
+
             res.render('registerPage.hbs', hbsObject);
             return;
           };
           next(err);
-        })
+        });
     }
   },
 }
