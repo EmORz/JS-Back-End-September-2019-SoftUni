52d13dd9b7d7879db463b8855fdf1dc36714199b
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-05-10 16:55:50 +0300

    Update home.js

--- Exams/Exam ExpressJS 19 April 2019/routers/home.js
+++ Exams/Exam ExpressJS 19 April 2019/routers/home.js
@@ -3,7 +3,7 @@ const controllers = require('../controllers');
 const auth = require('../utils/auth')
 
 router.get('/', auth(false), controllers.home.get.home);
-router.post('/', auth(false), controllers.home.post.search);
 
+router.post('/', auth(false), controllers.home.post.search);
 
 module.exports = router;
