372f0d371e1a48392134755d7d33cd729e35cc6a
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-05-10 16:54:06 +0300

    Refactoring course.js

--- Exams/Exam ExpressJS 19 April 2019/controllers/course.js
+++ Exams/Exam ExpressJS 19 April 2019/controllers/course.js
@@ -2,20 +2,27 @@ const models = require('../models');
 const config = require('../config/config');
 
 module.exports = {
+
   get: {
     create: function (req, res, next) {
+
       const hbsObject = {
         pageTitle: 'Create Page',
         isLoggedIn: req.cookies[config.cookie] !== undefined,
         username: req.user ? req.user.username : ''
       }
+
       res.render('createCoursePage.hbs', hbsObject)
     },
+    
     details: function (req, res, next) {
+
       const courseId = req.params.id;
       const userId = req.user.id;
+
       Promise.all(([models.Course.findById(courseId), models.User.findById(userId).populate('enrolledCourses')]))
         .then(([course, user]) => {
+
           const hbsObject = {
             pageTitle: 'Details Page',
             isLoggedIn: req.cookies[config.cookie] !== undefined,
@@ -24,13 +31,17 @@ module.exports = {
             isCreator: course.creatorId.toString() === user.id,
             isEnrolled: user.enrolledCourses.find(c => c.id === course.id)
           };
+
           res.render('detailsCoursePage.hbs', hbsObject);
         })
         .catch(error => next(error))
     },
+
     enroll: function (req, res, next) {
+
       const courseId = req.params.id;
       const userId = req.user.id;
+
       Promise.all([
         models.Course.updateOne({ _id: courseId }, { $push: { usersEnrolled: userId } }),
         models.User.updateOne({ _id: userId }, { $push: { enrolledCourses: courseId } })])
@@ -39,39 +50,54 @@ module.exports = {
         })
         .catch(next);
     },
+
     edit: function (req, res, next) {
+
       const courseId = req.params.id;
+
       models.Course.findById(courseId)
         .then(course => {
+
           const hbsObject = {
             pageTitle: 'Edit Page',
             isLoggedIn: req.cookies[config.cookie] !== undefined,
             username: req.user ? req.user.username : '',
             course: course
           };
+
           res.render('editCoursePage.hbs', hbsObject);
         })
         .catch(error => console.log(error))
     },
+
     delete: async function (req, res, next) {
+
       const courseId = req.params.id;
       const course = await models.Course.findById({ _id: courseId }).populate('usersEnrolled');
+
       course.usersEnrolled.map(async function (user) {
         await models.User.findByIdAndUpdate({ _id: user.id }, { $pull: { enrolledCourses: courseId } })
-      })
+      });
+
       await models.Course.findByIdAndRemove({ _id: courseId });
+
       res.redirect('/home/');
     }
   },
+
   post: {
     create: function (req, res, next) {
-      let { title, description, imageUrl, isPublic } = req.body;
-      isPublic = isPublic === 'on' ? true : false;
+
+      const { title, description, imageUrl } = req.body;
+      const isPublic = req.body.isPublic === 'on';
       const creatorId = req.user.id;
-      models.Course.create({ title, description, imageUrl, isPublic, creatorId, createdAt: new Date() })
+      const createdAt = new Date();
+
+      models.Course.create({ title, description, imageUrl, isPublic, creatorId, createdAt })
         .then(createdCourse => res.redirect('/home/'))
         .catch(err => {
           if (err.name === 'ValidationError') {
+
             const hbsObject = {
               pageTitle: 'Create Page',
               isLoggedIn: req.cookies[config.cookie] !== undefined,
@@ -79,20 +105,25 @@ module.exports = {
               course: { title, description, imageUrl, isPublic, creatorId },
               errors: err.errors
             };
+
             res.render('createCoursePage', hbsObject);
             return;
           };
+
           next(err);
-        })
+        });
     },
     edit: function (req, res, next) {
+
       const courseId = req.params.id;
-      let { title, description, imageUrl, isPublic } = req.body;
-      isPublic = isPublic === 'on';
+      const { title, description, imageUrl } = req.body;
+      const isPublic = req.body.isPublic === 'on';
+
       models.Course.updateOne({ _id: courseId }, { title, description, imageUrl, isPublic }, { runValidators: true })
         .then(() => res.redirect('/home/'))
         .catch(err => {
           if (err.name === 'ValidationError') {
+
             const hbsObject = {
               pageTitle: 'Edit Page',
               isLoggedIn: req.cookies[config.cookie] !== undefined,
@@ -100,11 +131,13 @@ module.exports = {
               course: { title, description, imageUrl, isPublic, _id: courseId },
               errors: err.errors
             };
+
             res.render('editCoursePage.hbs', hbsObject);
             return;
           };
+
           next(err);
-        })
+        });
     }
   }
 }
